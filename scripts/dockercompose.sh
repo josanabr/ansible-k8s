6a01e0ed67ee324fcfda5e78ad9cc5f85bda9f87
John Sanabria <john.sanabria@gmail.com>
2020-01-02 22:42:11 -0500

    avoiding output on screen during deployment process

--- scripts/dockercompose.sh
+++ scripts/dockercompose.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose >& docker-compose.curl.output
 sudo chmod +x /usr/local/bin/docker-compose
